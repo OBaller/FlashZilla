f87fd5a04e04e4f5cf17937fc8ceed70c404354c
brightsummit.co <naseemoyebola@gmail.com>
2022-02-17 19:56:20 +0100

    Initial Commit

---
+++ FlashZilla/FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by naseem on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
