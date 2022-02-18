52933eb082a0474a595a00d820967f3e7eed5782
brightsummit.co <naseemoyebola@gmail.com>
2022-02-18 23:44:10 +0100

    Swipe Colors

--- FlashZilla/FlashZilla/CardView.swift
+++ FlashZilla/FlashZilla/CardView.swift
@@ -10,11 +10,20 @@ import SwiftUI
 struct CardView: View {
     let card: Card
     @State private var isShowingAnswer = false
-    
+    @State private var offset = CGSize.zero
+    var removal: (() -> Void)? = nil
+
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(.white)
+                .fill(
+                    .white
+                        .opacity(1 - Double(abs(offset.width / 50)))
+                )
+                .background(
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
             
             VStack {
@@ -32,6 +41,23 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+        .offset(x: offset.width * 5, y: 0)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    offset = gesture.translation
+                }
+                .onEnded { _ in
+                    if abs(offset.width) > 100 {
+                        // remove the card
+                        removal?()
+                    } else {
+                        offset = .zero
+                    }
+                }
+        )
         .onTapGesture {
             isShowingAnswer.toggle()
         }
